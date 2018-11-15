ff33b2a8b597a43bec29089072aa6d8dbb6f1f25
Sergii Piatakov <sergii.piatakov@globallogic.com>
2018-11-15 15:45:44 +0200

    add a multiplication operation

--- calculator.cpp
+++ calculator.cpp
@@ -2,7 +2,12 @@
 
 int Calculator::Add (double a, double b)
 {
-
+// Dima is an apple of my eye 
+// Pinkod pinkodovich
+// Batko miy bendera
+// Ukraina matu
+// Zelenskiy prezident mira
+// Putin put hyi in himselfes mouse 
 	return a + b + 0.5;
 
 }
